c92d97a884437a1d39ab27a76aa108eef7779348
adithya3403 <a.dty.3403@gmail.com>
2023-06-05 16:53:41 +0530

    Create AboutUs.js

---
+++ src/components/AboutUs.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
+
+const AboutUs = () => {
+    return (
+        <div>
+            <>
+                <nav className="navbar">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <Link to="/" className="nav-link">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/About" className="nav-link">About</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/Contact" className="nav-link">Contact</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </>
+            <h1>About Us</h1>
+            <p>
+                Ipsum ipsum aute ea eiusmod.
+            </p>
+        </div>
+    );
+};
+
+export default AboutUs;
