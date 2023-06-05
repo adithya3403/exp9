8c1d84fb648ffe8b83989e0eb9aad5a492c8aa4b
adithya3403 <a.dty.3403@gmail.com>
2023-06-05 16:53:43 +0530

    Create ContactUs.js

---
+++ src/components/ContactUs.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
+
+const ContactUs = () => {
+    return (
+        <div>
+            <>
+                <nav className="navbar">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <Link to="/" className="nav-link">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/About" className="nav-link">About</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/Contact" className="nav-link">Contact</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </>
+            <h1>Contact Us</h1>
+            <p>
+                Consequat est ipsum cillum laboris sint nulla esse.
+            </p>
+            <ul>
+                <li>Email: contact@ourcompany.com</li>
+                <li>Phone: 123-456-7890</li>
+            </ul>
+        </div>
+    );
+};
+
+export default ContactUs;
